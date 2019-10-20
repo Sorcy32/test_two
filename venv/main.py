d7fdacb09dedecfd369b94656264504aa745223a
Sorcy32 <Sorcerer2005@yandex.ru>
2019-10-20 17:46:38 +0300

    added expansion check

--- venv/main.py
+++ venv/main.py
@@ -1,13 +1,13 @@
 '''
 Написать программу, которая
-1. принимает на вход в качестве именованного аргумента —input (-i) путь до считываемого файла
+#1. принимает на вход в качестве именованного аргумента —input (-i) путь до считываемого файла
 в формате <filename>.csv, в качестве второго аргумента  - —output (-o) - путь до целевого файла <filename>_odd.csv,
 Нужно ли различать ввели ли .csv?
 #2. сама программа считывает .csv,
 #3. оставляет только нечетные строки,
 #4. записывает их в целевой файл.
 #5. Если в файле ноль нечетных строк, целевой файл не создается, программа предупреждает об ошибке.
-6. Если недостаточно аргументов / не хватает файлов по указанному пути - программа сообщает об ошибке.
+#6. Если недостаточно аргументов / не хватает файлов по указанному пути - программа сообщает об ошибке.
 '''
 import argparse, csv, os
 
@@ -18,15 +18,38 @@ def main():
     parser.add_argument('-o', '--output', type=str, help='path for output file. Should be <filename>_odd.csv')
     args = parser.parse_args()
 
-    # Проверка передали ли путь к файлу с расширением или без
-    print('test', args.input.rsplit('.')[1])
-
     #Проверяем передали ли нам все необходимые аргументы (input & output)
     if args.input == None or args.output == None:
-        return(print('Не хватает входящих аргументов'))
+        print('Не хватает входящих аргументов')
+        os._exit(0)
+
+    # Проверка передали ли путь к файлу с расширением или без, верно ли указан output_odd.csv
+    try:
+        if args.input.rsplit('.')[1] != 'csv':    #попытка получить расширение файла
+            (print("Расширение файла импорта должно быть CSV"))
+            os._exit(0)
+    except IndexError: #если расширение не указано - присвоим
+        args.input = args.input + '.csv'
+    try:
+        if args.output.rsplit('.')[1] != 'csv':
+            print("Расширение файла экспорта должно быть CSV")
+            os._exit(0)
+    except IndexError: #Если расширение не указано - присваиваем.
+        args.output = args.output.rsplit('.')[0] + '.csv'
+    #Если указано окончание файла не '_odd.csv' - добавим.
+    if args.output[-8:] != '_odd.csv':
+        args.output = args.output[0:-4] + '_odd.csv'
+
     #Проверка наличия существования сходного файла. Выходной проверять не будем. Он создается при необходимости
     if os.path.exists(args.input) == False:
-        return (print(('Указанный входящий файл не сушествует')))
+        print(('Указанный входящий файл для импорта не сушествует'))
+        os._exit(0)
+
+    if os.path.dirname(args.output[0:-4]) != True:
+        try:
+            os.makedirs(os.path.dirname(args.output[0:-4]))
+        except FileExistsError:
+            pass
     return args.input, args.output
 
 
@@ -41,33 +64,27 @@ def sort_save(input_path, output_path):
                 if counter%2 != 0:
                     data_to_save.append(row)
                 counter+=1
-    except: return (print('Ошибка при чтении файла'))
+    except:
+        (print('Ошибка при чтении файла'))
+        os._exit(0)
 
     #Проверяем не равно ли количество четных строк нулю
     if len(data_to_save)==0:
-        return print('Нечетных строк ноль!') #Если равно нулю = следующий блок с созданием файла не сработает
+         print('Нечетных строк ноль!') #Если равно нулю = следующий блок с созданием файла не сработает
+         os._exit(0)
     #Если не равно нулю - записываем в выходной файл
     else:
         try:
             with open (output_path, 'w', newline='', encoding='utf-8') as out_file:
                 writer = csv.writer(out_file)
                 writer.writerows(data_to_save)
-            print('Успешно сохранено в файл. ')
-        except:            print('Ошибка при записи файла')
-
-
-
-
-
-
-
-
-
-
-
+        except:
+            print('Ошибка при записи файла')
+            os._exit(0)
+    return output_path
 
 if __name__ == "__main__":
-    input_path,output_path = main()
+    input_path , output_path = main()
     sort_save(input_path,output_path)
 
-print('Программа доработала до конца')
+print('Выполнено.  Можно открыть файл ' , output_path)
